e902fc02fa117feddb0b9fcfd175d33ac99b6ead
bharatrajput2409 <nutboltcode@gmail.com>
2021-03-08 02:58:43 +0530

    adding blog url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./components/home";
 import Members from "./components/members/member";
 import Footer from "./components/footer";
 import Events from "./components/Events/Event";
+import blogApp from './components/Blogs/blogApp'
+import pagenotfound from './components/pagenotfound'
 // import Timeline from "./components/Timeline/timeline";
 class App extends React.Component {
   render() {
@@ -17,6 +19,8 @@ class App extends React.Component {
                 <Route exact path="/members" component={Members} />
                 <Route exact path="/events" component={Events} />
                 {/* <Route exact path="/timeline" component={Timeline} /> */}
+                <Route path="/blog" component={blogApp}/>
+                <Route component={pagenotfound}/>
               </Switch>
             </Router>
           </main>
