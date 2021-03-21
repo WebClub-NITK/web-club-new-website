d9a3499331ab3e4dd04887c5869bc667baa26c2d
bharatrajput2409 <nutboltcode@gmail.com>
2021-03-21 12:09:29 +0530

    removing backend

--- webclubBackend/blogs/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import blogs,tag,taginblog
-# Register your models here.
-admin.site.register(blogs)
-admin.site.register(tag)
-admin.site.register(taginblog)
