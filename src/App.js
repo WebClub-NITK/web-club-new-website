0b0c797da69f5340ab00eeaaf8110806d09359ba
bharatrajput2409 <nutboltcode@gmail.com>
2021-03-25 18:04:10 +0530

    adding lazy load

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, {Suspense, lazy } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/home";
 import Members from "./components/members/member";
 import Footer from "./components/footer";
 import Events from "./components/Events/Event";
-import blogApp from './components/Blogs/blogApp'
 import pagenotfound from './components/pagenotfound'
+import Loader from "react-loader-spinner";
+const BlogApp = lazy(() => import('./components/Blogs/BlogApp'));
 // import Timeline from "./components/Timeline/timeline";
 class App extends React.Component {
   render() {
@@ -14,18 +15,20 @@ class App extends React.Component {
         <div id="wrapper" className="d-flex flex-column">
           <main className="flex-fill">
             <Router>
+              <Suspense fallback={<div className="d-flex justify-content-center"><Loader type="TailSpin" color="#00BFFF" height={100} width={100} /></div>}>
               <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/members" component={Members} />
                 <Route exact path="/events" component={Events} />
                 {/* <Route exact path="/timeline" component={Timeline} /> */}
-                <Route path="/blog" component={blogApp}/>
-                <Route component={pagenotfound}/>
+                <Route path="/blog" component={BlogApp} />
+                <Route component={pagenotfound} />
               </Switch>
+              </Suspense>
             </Router>
           </main>
-          <Footer />
-        </div>
+        <Footer />
+      </div>
       </>
     );
   }
